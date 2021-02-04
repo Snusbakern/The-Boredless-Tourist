ff3813fb96a36e9857ba1a5fbffed53c9c78a196
Andre <andre_aakerholm@hotmail.com>
2021-02-05 00:54:35 +0100

    Added function to generate message for traveler and present attractions they might be interested in.

--- script.py
+++ script.py
@@ -9,9 +9,7 @@ def get_traveler_location(traveler):
     traveler_destination_index = get_destination_index(traveler_destination)
     return traveler_destination_index
 test_destination_index = get_traveler_location(test_traveler)
-print(test_destination_index)
 attractions = [[] for i in destinations]
-print(attractions)
 def add_attraction(destination, attraction):
     try:
         destination_index = get_destination_index(destination)
@@ -21,10 +19,9 @@ def add_attraction(destination, attraction):
     attractions_for_destination.append(attraction)
     return
 add_attraction('Los Angeles, USA', ['Venice Beach', ['beach']])
-print(attractions)
 add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
 add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
-add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historicical site"]])
+add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historical site"]])
 add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
 add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
 add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
@@ -40,8 +37,27 @@ def find_attractions(destination, interests):
         possible_attraction = i
         attraction_tags = possible_attraction[1]
         for n in interests:
-            if n == attraction_tags[0]:
+            if n in attraction_tags:
                 attractions_with_interest.append(possible_attraction[0])
     return attractions_with_interest
 la_arts = find_attractions('Los Angeles, USA', ['art'])
-print(la_arts)
+def get_attraction_for_traveler(traveler):
+    traveler_destination = traveler[1]
+    traveler_interests = traveler[2]
+    traveler_attractions = find_attractions(traveler_destination, traveler_interests)
+    interests_string = 'Hi '
+    interests_string += traveler[0]
+    interests_string += ", we think you'll like these places around "
+    interests_string += traveler_destination
+    interests_string += ': '
+    def list_to_string(s):
+        str1 = ''
+        for i in s:
+            str1 += i + '"'
+            if i != s[-1]:
+                str1 += ', '
+        return str1
+    interests_string += list_to_string(traveler_attractions)
+    return interests_string
+smills_france = get_attraction_for_traveler(['Dereck Smill', 'Paris, France', ['monument']])
+print(smills_france)
